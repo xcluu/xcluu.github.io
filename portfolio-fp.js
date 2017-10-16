3a1e4eb6c636d5568a3fdc8a51b101b8ecea8574
Claudia Wegen <claudia.wegen@hs-furtwangen.de>
2017-10-16 10:47:13 +0200

    Add files via upload

---
+++ portfolio-fp.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Claudia on 18.07.2017.
+ */
+
+// $(document).ready(function() {
+//     $('#fullpage').fullpage();
+// });
+//header wird geladen ohne Hintergrund, grauer overlay und man kann nicht runterscrollen.
+//funktioniert wahrscheinlich nicht weil die Slideshow-Bilder erst beim sliden geladen werden??
---
+++ portfolio.ts
@@ -0,0 +1,50 @@
+/**
+ * Created by Claudia on 17.07.2017.
+ */
+
+let slideIndex = 1;
+
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide") as HTMLCollectionOf<HTMLElement>;
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    console.log(slides);
+    console.log(slides[(slideIndex-1)]);
+    console.log(slideIndex)
+    slides[(slideIndex-1)].style.display = "block";
+    dots[(slideIndex-1)].className += " active";
+}
+
+//ruft Funktion nicht auf? in Konsole wird nichts angezeigt egal wie ich die Funktion aufrufe
+window.onscroll = function(){
+    arrowHide();
+}
+
+function arrowHide() {
+    let height=window.innerHeight;
+    console.log("test");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("to-top").style.display = "block";
+    } else {
+        document.getElementById("to-top").style.display = "none";
+    }
+}
